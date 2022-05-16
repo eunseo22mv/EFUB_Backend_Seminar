48e00a60fed2dfc6b6b3c384873cd434cde69115
Eunseo Kim <70934572+eunseo22mv@users.noreply.github.com>
2022-05-16 23:27:03 +0900

    HW1 프로젝트 업로드

---
+++ 0510_Test Code/HW1/src/test/java/eunseo/seminar/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package eunseo.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
